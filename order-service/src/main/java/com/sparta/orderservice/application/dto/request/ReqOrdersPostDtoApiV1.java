bb7f0a3275dcc089575fb984a0ce908fb52d6f28
djmachine <qw1310@naver.com>
2025-04-08 11:09:51 +0900

    [feat #31-order-create] 주문 create  기능 구현

---
+++ order-service/src/main/java/com/sparta/orderservice/application/dto/request/ReqOrdersPostDtoApiV1.java
@@ -0,0 +1,28 @@
+package com.sparta.orderservice.application.dto.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ReqOrdersPostDtoApiV1 {
+
+    @Valid
+    @NotNull(message = "주문 정보를 입력해주세요.")
+    private Order order;
+
+    @Getter
+    @Builder
+    public static class Order {
+
+        @NotBlank(message = "슬랙 정보를 입력해주세요.")
+        private String slackId;
+
+        @NotBlank(message = "주문제품의 수량을 입력해주세요.")
+        private int orderQuantity;
+    }
+}
+

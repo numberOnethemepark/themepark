346ffba05b444c24cfc265f99f5c6b4aff6b3227
devdaeun <ekdms008@gmail.com>
2025-04-08 19:47:20 +0900

    [feat #22-waiting-read] 테마파크대기 Read 기능 구현 - read api 구현 - read api 관련 response Dto 구현 - 테스트 코드 작성

--- themepark-service/src/main/java/com/business/themeparkservice/waiting/presentation/controller/WaitingControllerApiV1.java
+++ themepark-service/src/main/java/com/business/themeparkservice/waiting/presentation/controller/WaitingControllerApiV1.java
@@ -1,12 +1,15 @@
 package com.business.themeparkservice.waiting.presentation.controller;
 
 import com.business.themeparkservice.waiting.application.dto.request.ReqWaitingPostDTOApiV1;
+import com.business.themeparkservice.waiting.application.dto.response.ResWaitingGetByIdDTOApiV1;
 import com.business.themeparkservice.waiting.application.dto.response.ResWaitingPostDTOApiV1;
 import com.business.themeparkservice.waiting.common.dto.ResDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/waitings")
@@ -23,6 +26,18 @@ public class WaitingControllerApiV1 {
                         .data(ResWaitingPostDTOApiV1.of(reqDto))
                         .build(),
                 HttpStatus.CREATED
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResDTO<ResWaitingGetByIdDTOApiV1>>getWaiting(@PathVariable UUID id){
+        return new ResponseEntity<>(
+                ResDTO.<ResWaitingGetByIdDTOApiV1>builder()
+                        .code(0)
+                        .message("대기열 조회에 성공했습니다.")
+                        .data(ResWaitingGetByIdDTOApiV1.of(id))
+                        .build(),
+                HttpStatus.OK
         );
     }
 

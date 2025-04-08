bfdbe1554b4e339e6ef8d76a35ba5b29658bf4e8
devdaeun <ekdms008@gmail.com>
2025-04-08 15:18:36 +0900

    [feat #22-hashtag-read] 해시태그 Read 기능 구현 - Read api 추가 - ResponseDto 추가 - 테스트 코드 작성

--- themepark-service/src/main/java/com/business/themeparkservice/hashtag/presentation/controller/HashtagControllerApiV1.java
+++ themepark-service/src/main/java/com/business/themeparkservice/hashtag/presentation/controller/HashtagControllerApiV1.java
@@ -1,15 +1,15 @@
 package com.business.themeparkservice.hashtag.presentation.controller;
 
 import com.business.themeparkservice.hashtag.application.dto.request.ReqHashtagPostDTOApiV1;
+import com.business.themeparkservice.hashtag.application.dto.response.ResHashtagGetByIdDTOApiV1;
 import com.business.themeparkservice.hashtag.application.dto.response.ResHashtagPostDTOApiV1;
 import com.business.themeparkservice.themepark.common.dto.ResDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/hashtags")
@@ -24,6 +24,18 @@ public class HashtagControllerApiV1 {
                         .data(ResHashtagPostDTOApiV1.of(reqDto))
                         .build(),
                 HttpStatus.CREATED
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResDTO<ResHashtagGetByIdDTOApiV1>> getHashtagById(@PathVariable UUID id){
+        return new ResponseEntity<>(
+                ResDTO.<ResHashtagGetByIdDTOApiV1>builder()
+                        .code(0)
+                        .message("해시태그 조회에 성공했습니다.")
+                        .data(ResHashtagGetByIdDTOApiV1.of(id))
+                        .build(),
+                HttpStatus.OK
         );
     }
 }

e3727dedc4736fb39704e46c4afdeb62b7952a3d
문고은 <>
2025-04-08 15:40:23 +0900

    [feat #25-user-read] 회원 아이디 기반 회원 정보 조회 controller, dto 구현

--- user-service/src/main/java/com/business/userservice/presentation/controller/UserControllerApiV1.java
+++ user-service/src/main/java/com/business/userservice/presentation/controller/UserControllerApiV1.java
@@ -1,10 +1,12 @@
 package com.business.userservice.presentation.controller;
 
 import com.business.userservice.application.dto.request.ReqUserPutDTOApiV1;
+import com.business.userservice.application.dto.response.ResUserGetByIdDTOApiV1;
 import com.business.userservice.common.dto.ResDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +26,21 @@ public class UserControllerApiV1 {
             ResDTO.builder()
                 .code(0)
                 .message("회원 정보 수정에 성공했습니다.")
+                .build(),
+            HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResDTO<ResUserGetByIdDTOApiV1>> getById(
+        @PathVariable Long id
+    ) {
+        ResUserGetByIdDTOApiV1 tempResDto = ResUserGetByIdDTOApiV1.of("username", "slack-id");
+        return new ResponseEntity<>(
+            ResDTO.<ResUserGetByIdDTOApiV1>builder()
+                .code(0)
+                .message("회원 조회에 성공하였습니다.")
+                .data(tempResDto)
                 .build(),
             HttpStatus.OK
         );

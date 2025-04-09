12fd15be4af0e5b9e0c8589718d0cfe712e35f7c
devdaeun <ekdms008@gmail.com>
2025-04-09 11:33:54 +0900

    [feat #22-hashtag-search] 테마파크해시태그 Search 기능 구현 - search api 구현 - search api 관련 resposneDto 생성 - HashtagEntity 생성 - 테스트 코드 작성

--- themepark-service/src/main/java/com/business/themeparkservice/hashtag/presentation/controller/HashtagControllerApiV1.java
+++ themepark-service/src/main/java/com/business/themeparkservice/hashtag/presentation/controller/HashtagControllerApiV1.java
@@ -3,14 +3,21 @@ package com.business.themeparkservice.hashtag.presentation.controller;
 import com.business.themeparkservice.hashtag.application.dto.request.ReqHashtagPostDTOApiV1;
 import com.business.themeparkservice.hashtag.application.dto.request.ReqHashtagPutDTOApiV1;
 import com.business.themeparkservice.hashtag.application.dto.response.ResHashtagGetByIdDTOApiV1;
+import com.business.themeparkservice.hashtag.application.dto.response.ResHashtagGetDTOApiV1;
 import com.business.themeparkservice.hashtag.application.dto.response.ResHashtagPostDTOApiV1;
 import com.business.themeparkservice.hashtag.application.dto.response.ResHashtagPutDTOApiV1;
+import com.business.themeparkservice.hashtag.domain.entity.HashtagEntity;
 import com.business.themeparkservice.themepark.common.dto.ResDTO;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -39,6 +46,31 @@ public class HashtagControllerApiV1 {
                         .build(),
                 HttpStatus.OK
         );
+    }
+
+    @GetMapping
+    public ResponseEntity<ResDTO<ResHashtagGetDTOApiV1>> getHashtag(
+            @RequestParam(required = false) String name,
+            @PageableDefault(page = 0, size = 10, sort = "createdAt") Pageable pageable
+    ){
+        List<HashtagEntity> tempHashtags = List.of(
+                new HashtagEntity(),
+                new HashtagEntity()
+        );
+
+        Page<HashtagEntity> tempHashtagPage = new PageImpl<>(
+                tempHashtags, pageable, tempHashtags.size()
+        );
+
+        return new ResponseEntity<>(
+                ResDTO.<ResHashtagGetDTOApiV1>builder()
+                        .code(0)
+                        .message("해시태그 검색에 성공했습니다.")
+                        .data(ResHashtagGetDTOApiV1.of(tempHashtagPage))
+                        .build(),
+                HttpStatus.OK
+        );
+
     }
 
     @PutMapping("/{id}")

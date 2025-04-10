44256c4b93e1338e38989bdcb0d0e40a80fab503
djmachine <qw1310@naver.com>
2025-04-10 10:57:55 +0900

    [test /#74-payment-test] 결제 test 기능 구현

--- order-service/src/test/java/com/sparta/orderservice/presentation/controller/PaymentControllerApiV1Test.java
+++ order-service/src/test/java/com/sparta/orderservice/presentation/controller/PaymentControllerApiV1Test.java
@@ -1,6 +1,7 @@
 package com.sparta.orderservice.presentation.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,5 +20,14 @@ public class PaymentControllerApiV1Test {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Test
+    public void testPaymentGetSuccess() throws Exception {}
+
+    @Test
+    public void testPaymentUpdateSuccess() throws Exception {}
+
+    @Test
+    public void testPaymentPostSuccess() throws Exception {}
 
 }

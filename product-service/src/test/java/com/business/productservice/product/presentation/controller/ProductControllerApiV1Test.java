10f4b5baab1eec64e701cb4a1c48142f94936921
dowon0113 <dowon1235@naver.com>
2025-04-10 16:02:27 +0900

    [docs #76-product-stock] 상품 생성 API RestDocs 문서화 및 스웨거 자동화 적용
    - testProductPostSuccess()에 .andDo()추가
    - API 문서 자동 생성을 위한 RestDocs + Swagger 연동 작업
    - tag: Product v1 설정

--- product-service/src/test/java/com/business/productservice/product/presentation/controller/ProductControllerApiV1Test.java
+++ product-service/src/test/java/com/business/productservice/product/presentation/controller/ProductControllerApiV1Test.java
@@ -6,22 +6,31 @@ import com.business.productservice.application.dto.request.ReqStockDecreasePostD
 import com.business.productservice.application.dto.request.ReqStockRestorePostDTOApiV1;
 import com.business.productservice.domain.product.vo.ProductStatus;
 import com.business.productservice.domain.product.vo.ProductType;
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.SimpleType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
 @SpringBootTest
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc
 @Transactional
 @ActiveProfiles("dev")
@@ -50,33 +59,66 @@ public class ProductControllerApiV1Test {
                 .build();
 
         mockMvc.perform(
-                MockMvcRequestBuilders.post("/v1/products")
+                RestDocumentationRequestBuilders.post("/v1/products")
                         .content(dtoToJson(dto))
                         .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpectAll(
                         MockMvcResultMatchers.status().isCreated()
+                )
+                .andDo(
+                        MockMvcRestDocumentationWrapper.document("상품 생성 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Product v1")
+                                        .build()
+                                )
+                        )
                 );
     }
 
     @Test
     public void testProductGetByIdSuccess() throws Exception {
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/v1/products/{id}", UUID.randomUUID())
-        )
-        .andExpectAll(
-                MockMvcResultMatchers.status().isOk()
-        );
+                        RestDocumentationRequestBuilders.get("/v1/products/{id}", UUID.randomUUID())
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk()
+                )
+                .andDo(
+                        MockMvcRestDocumentationWrapper.document("상품 개별 조회 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Product v1")
+                                        .pathParameters(
+                                                parameterWithName("id").type(SimpleType.STRING).description("상품 ID")
+                                        )
+                                        .build()
+                                )
+                        )
+                );
     }
 
     @Test
     public void testProductGetSuccess() throws Exception {
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/v1/products")
-            )
-            .andExpectAll(
-                MockMvcResultMatchers.status().isOk()
-            );
+                        RestDocumentationRequestBuilders.get("/v1/products")
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk()
+                )
+                .andDo(
+                        MockMvcRestDocumentationWrapper.document("상품 전체 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Product v1")
+                                        .build()
+                                )
+                        )
+                );
     }
 
     @Test
@@ -97,12 +139,25 @@ public class ProductControllerApiV1Test {
                 .build();
 
         mockMvc.perform(
-                MockMvcRequestBuilders.put("/v1/products/{id}",UUID.randomUUID())
+                        RestDocumentationRequestBuilders.put("/v1/products/{id}",UUID.randomUUID())
                         .content(dtoToJson(dto))
                         .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpectAll(
                         MockMvcResultMatchers.status().isOk()
+                )
+                .andDo(
+                        MockMvcRestDocumentationWrapper.document("상품 수정 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Product v1")
+                                        .pathParameters(
+                                                parameterWithName("id").type(SimpleType.STRING).description("상품 ID")
+                                        )
+                                        .build()
+                                )
+                        )
                 );
 
     }
@@ -110,10 +165,23 @@ public class ProductControllerApiV1Test {
     @Test
     public void testProductDeleteSuccess() throws Exception {
         mockMvc.perform(
-                MockMvcRequestBuilders.delete("/v1/products/{id}", UUID.randomUUID())
+                        RestDocumentationRequestBuilders.delete("/v1/products/{id}", UUID.randomUUID())
                 )
                 .andExpectAll(
                         MockMvcResultMatchers.status().isOk()
+                )
+                .andDo(
+                        MockMvcRestDocumentationWrapper.document("상품 삭제 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Product v1")
+                                        .pathParameters(
+                                                parameterWithName("id").type(SimpleType.STRING).description("상품 ID")
+                                        )
+                                        .build()
+                                )
+                        )
                 );
     }
 
@@ -129,12 +197,25 @@ public class ProductControllerApiV1Test {
                 .build();
 
         mockMvc.perform(
-                        MockMvcRequestBuilders.post("/v1/products/{id}/stocks-decrease", UUID.randomUUID())
+                        RestDocumentationRequestBuilders.post("/v1/products/{id}/stocks-decrease", UUID.randomUUID())
                                 .content(dtoToJson(dto))
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpectAll(
                         MockMvcResultMatchers.status().isOk()
+                )
+                .andDo(
+                        MockMvcRestDocumentationWrapper.document("상품 재고 차감 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Product v1")
+                                        .pathParameters(
+                                                parameterWithName("id").type(SimpleType.STRING).description("상품 ID")
+                                        )
+                                        .build()
+                                )
+                        )
                 );
     }
 
@@ -149,13 +230,27 @@ public class ProductControllerApiV1Test {
                 )
                 .build();
         mockMvc.perform(
-                MockMvcRequestBuilders.post("/v1/products/{id}/stocks-restore", UUID.randomUUID())
+                        RestDocumentationRequestBuilders.post("/v1/products/{id}/stocks-restore", UUID.randomUUID())
                         .content(dtoToJson(dto))
                         .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpectAll(
                         MockMvcResultMatchers.status().isOk()
+                )
+                .andDo(
+                        MockMvcRestDocumentationWrapper.document("상품 재고 복구 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Product v1")
+                                        .pathParameters(
+                                                parameterWithName("id").type(SimpleType.STRING).description("상품 ID")
+                                        )
+                                        .build()
+                                )
+                        )
                 );
+
     }
 
     private String dtoToJson(Object dto) throws Exception {

f8d0892622e39c8fd15182ba3a76b6b1cc2bb883
devdaeun <ekdms008@gmail.com>
2025-04-10 15:32:08 +0900

    [docs #89-themepark] 테마파크 swagger 자동 문서화기능 추가 - build.gradle 에 swagger 의존성 추가 - 테마파크 테스트코드 swagger 문서화 연결 - 해시태그 테스트코드 swagger 문서화 연결 - 대기 테스트코드 swagger 문서화 연결

--- themepark-service/src/test/java/com/business/themeparkservice/waiting/presentation/controller/WaitingControllerApiV1Test.java
+++ themepark-service/src/test/java/com/business/themeparkservice/waiting/presentation/controller/WaitingControllerApiV1Test.java
@@ -1,12 +1,17 @@
 package com.business.themeparkservice.waiting.presentation.controller;
 
 import com.business.themeparkservice.waiting.application.dto.request.ReqWaitingPostDTOApiV1;
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.SimpleType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -15,8 +20,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 @Transactional
 @ActiveProfiles("dev")
 public class WaitingControllerApiV1Test {
@@ -40,62 +51,128 @@ public class WaitingControllerApiV1Test {
         String reqJsonDto = objectMapper.writeValueAsString(reqWaitingPostDTOApiV1);
 
         mockMvc.perform(
-                MockMvcRequestBuilders.post("/v1/waitings")
+                RestDocumentationRequestBuilders.post("/v1/waitings")
                         .content(reqJsonDto)
                         .contentType(MediaType.APPLICATION_JSON)
         )
                 .andExpectAll(
                         MockMvcResultMatchers.status().isCreated()
+                ).andDo(
+                        MockMvcRestDocumentationWrapper.document("Waiting 생성 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Waiting v1")
+                                        .build()
+                                )
+                        )
                 );
     }
 
     @Test
     public void testWaitingGetByIdSuccess() throws Exception{
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/v1/waitings/{id}", UUID.randomUUID())
+                RestDocumentationRequestBuilders.get("/v1/waitings/{id}", UUID.randomUUID())
         )
                 .andExpectAll(
                         MockMvcResultMatchers.status().isOk()
+                ).andDo(
+                        MockMvcRestDocumentationWrapper.document("Waiting 단일 조회 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Waiting v1")
+                                        .pathParameters(
+                                                parameterWithName("id").type(SimpleType.STRING).description("대기 ID")
+                                        )
+                                        .build()
+                                )
+                        )
                 );
     }
 
     @Test
     public void testWaitingGetSuccess() throws Exception{
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/v1/waitings")
+                RestDocumentationRequestBuilders.get("/v1/waitings")
         )
                 .andExpectAll(
                         MockMvcResultMatchers.status().isOk()
+                ).andDo(
+                        MockMvcRestDocumentationWrapper.document("Waiting 전체 조회 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Waiting v1")
+                                        .build()
+                                )
+                        )
                 );
     }
 
     @Test
     public void testWaitingPostDoneSuccess() throws Exception{
         mockMvc.perform(
-                MockMvcRequestBuilders.post("/v1/waitings/{id}", UUID.randomUUID()+"/done")
+                RestDocumentationRequestBuilders.post("/v1/waitings/{id}/done", UUID.randomUUID())
         )
                 .andExpectAll(
                         MockMvcResultMatchers.status().isOk()
+                ).andDo(
+                        MockMvcRestDocumentationWrapper.document("Waiting 완료 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Waiting v1")
+                                        .pathParameters(
+                                                parameterWithName("id").type(SimpleType.STRING).description("대기 ID")
+                                        )
+                                        .build()
+                                )
+                        )
                 );
     }
 
     @Test
     public void testWaitingPostCancelSuccess() throws Exception{
         mockMvc.perform(
-                        MockMvcRequestBuilders.post("/v1/waitings/{id}", UUID.randomUUID()+"/cancel")
-                )
+                        RestDocumentationRequestBuilders.post("/v1/waitings/{id}/cancel", UUID.randomUUID())
+        )
                 .andExpectAll(
                         MockMvcResultMatchers.status().isOk()
+                ).andDo(
+                        MockMvcRestDocumentationWrapper.document("Waiting 취소 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Waiting v1")
+                                        .pathParameters(
+                                                parameterWithName("id").type(SimpleType.STRING).description("대기 ID")
+                                        )
+                                        .build()
+                                )
+                        )
                 );
     }
 
     @Test
     public void testWaitingGetDeletedSuccess() throws Exception{
         mockMvc.perform(
-                MockMvcRequestBuilders.delete("/v1/waitings/{id}", UUID.randomUUID())
+                RestDocumentationRequestBuilders.delete("/v1/waitings/{id}", UUID.randomUUID())
         )
                 .andExpectAll(
                         MockMvcResultMatchers.status().isOk()
+                ).andDo(
+                        MockMvcRestDocumentationWrapper.document("Waiting 삭제 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("Waiting v1")
+                                        .pathParameters(
+                                                parameterWithName("id").type(SimpleType.STRING).description("대기 ID")
+                                        )
+                                        .build()
+                                )
+                        )
                 );
     }
 }

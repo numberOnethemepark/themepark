22c0d08b00a99881eae195a5a94fa00c5d634b40
dowon0113 <dowon1235@naver.com>
2025-04-11 17:17:32 +0900

    [feat/#104-product] 엔티티 생성
    - 상품 엔티티 생성

--- product-service/src/main/java/com/business/productservice/domain/product/entity/ProductEntity.java
+++ product-service/src/main/java/com/business/productservice/domain/product/entity/ProductEntity.java
@@ -1,4 +1,41 @@
 package com.business.productservice.domain.product.entity;
 
+import com.business.productservice.domain.product.vo.ProductStatus;
+import com.business.productservice.domain.product.vo.ProductType;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(name = "p_products")
 public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "product_id", nullable = false)
+    private UUID id;
+
+    @Column(name = "product_name", nullable = false)
+    private String name;
+
+    @Column(name = "product_description", nullable = false)
+    private String description;
+
+    @Column(name = "product_type", nullable = false)
+    private ProductType productType;
+
+    @Column(name = "product_status", nullable = false)
+    private ProductStatus status;
+
+    @Column(name = "limit_quantity", nullable = false)
+    private Integer limitQuantity;
+
+    @Column(name = "event-start-at", nullable = false)
+    private LocalDateTime eventStartAt;
+
+    @Column(name = "event-end-at", nullable = false)
+    private LocalDateTime eventEndTAt;
 }

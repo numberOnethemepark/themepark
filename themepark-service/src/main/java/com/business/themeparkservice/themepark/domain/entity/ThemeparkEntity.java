13a18eb595d793a8483d5b123366a6d4fe2c2558
devdaeun <ekdms008@gmail.com>
2025-04-11 11:50:27 +0900

    [feat/#100-themepark] 테마파크 entity,repository 추가 - postgreSQL 의존성 추가 - 테마파크 entity 추가 - 테마파크 repository 추가

--- themepark-service/src/main/java/com/business/themeparkservice/themepark/domain/entity/ThemeparkEntity.java
+++ themepark-service/src/main/java/com/business/themeparkservice/themepark/domain/entity/ThemeparkEntity.java
@@ -1,4 +1,42 @@
 package com.business.themeparkservice.themepark.domain.entity;
 
+import com.business.themeparkservice.themepark.domain.vo.ThemeparkType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Getter
 public class ThemeparkEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "themepark_id", nullable = false)
+    private UUID id;
+
+    @Column(name = "themepark_name", nullable = false)
+    private String name;
+
+    @Column(name = "themepark_description", nullable = false)
+    private String description;
+
+    @Column(name = "themepark_type", nullable = false)
+    private ThemeparkType type;
+
+    @Column(name = "operation_start_time", nullable = false)
+    private LocalTime operationStartTime;
+
+    @Column(name = "operation_end_time", nullable = false)
+    private LocalDateTime operationEndTime;
+
+    @Column(name = "height_limit", nullable = false)
+    private String heightLimit;
+
+    @Column(name = "supervisor", nullable = false)
+    @ColumnDefault("false")
+    private boolean supervisor;
+
 }
---
+++ themepark-service/src/main/java/com/business/themeparkservice/themepark/infastructure/ThemeparkJpaRepository.java
@@ -0,0 +1,12 @@
+package com.business.themeparkservice.themepark.infastructure;
+
+import com.business.themeparkservice.themepark.domain.entity.ThemeparkEntity;
+import com.business.themeparkservice.themepark.domain.repository.ThemeparkRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ThemeparkJpaRepository extends JpaRepository<ThemeparkEntity, UUID>, ThemeparkRepository {
+}

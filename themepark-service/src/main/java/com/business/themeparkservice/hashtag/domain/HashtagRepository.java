b426eebc99d502b4d229e00e0df9ff30a8eced66
devdaeun <ekdms008@gmail.com>
2025-04-11 15:46:04 +0900

    [feat/#100-themepark] 테마파크 해시태그 entity,repository 추가 - 패키지 경로 수정

--- themepark-service/src/main/java/com/business/themeparkservice/hashtag/domain/HashtagRepository.java
+++
@@ -1,4 +0,0 @@
-package com.business.themeparkservice.hashtag.domain;
-
-public interface HashtagRepository {
-}
--- themepark-service/src/main/java/com/business/themeparkservice/hashtag/infastructure/persistence/hashtag/HashtagJpaRepository.java
+++ themepark-service/src/main/java/com/business/themeparkservice/hashtag/infastructure/persistence/hashtag/HashtagJpaRepository.java
@@ -1,6 +1,6 @@
 package com.business.themeparkservice.hashtag.infastructure.persistence.hashtag;
 
-import com.business.themeparkservice.hashtag.domain.HashtagRepository;
+import com.business.themeparkservice.hashtag.domain.repository.HashtagRepository;
 import com.business.themeparkservice.hashtag.domain.entity.HashtagEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

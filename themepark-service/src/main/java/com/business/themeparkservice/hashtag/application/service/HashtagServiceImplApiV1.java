9eaf518ccf8167a0d71749345d6dbc206715db0b
devdaeun <ekdms008@gmail.com>
2025-04-14 01:50:24 +0900

    [feat/#119-hashtag-read] 테마파크해시태그 조회 기능 구현 - controller 수정 - service 로직추가 - response dto 수정

--- themepark-service/src/main/java/com/business/themeparkservice/hashtag/application/service/HashtagServiceImplApiV1.java
+++ themepark-service/src/main/java/com/business/themeparkservice/hashtag/application/service/HashtagServiceImplApiV1.java
@@ -1,12 +1,16 @@
 package com.business.themeparkservice.hashtag.application.service;
 
 import com.business.themeparkservice.hashtag.application.dto.request.ReqHashtagPostDTOApiV1;
+import com.business.themeparkservice.hashtag.application.dto.response.ResHashtagGetByIdDTOApiV1;
 import com.business.themeparkservice.hashtag.application.dto.response.ResHashtagPostDTOApiV1;
 import com.business.themeparkservice.hashtag.domain.entity.HashtagEntity;
 import com.business.themeparkservice.hashtag.infastructure.persistence.hashtag.HashtagJpaRepository;
+import jakarta.ws.rs.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +23,16 @@ public class HashtagServiceImplApiV1 implements HashtagServiceApiV1{
     public ResHashtagPostDTOApiV1 postBy(ReqHashtagPostDTOApiV1 reqDto) {
         HashtagEntity hashtagEntity = hashtagRepository.save(reqDto.createHashtag());
         return ResHashtagPostDTOApiV1.of(hashtagEntity);
+    }
+
+    @Override
+    public ResHashtagGetByIdDTOApiV1 getBy(UUID id) {
+        HashtagEntity hashtagEntity = getHashtag(id);
+        return ResHashtagGetByIdDTOApiV1.of(hashtagEntity);
+    }
+
+    private HashtagEntity getHashtag(UUID id) {
+        return hashtagRepository.findById(id).orElseThrow(()
+                -> new NotFoundException("해시태그가 존재하지않습니다."));
     }
 }

df15dd312c93a407cd523c699d592be4cfdc7e94
devdaeun <ekdms008@gmail.com>
2025-04-13 21:47:35 +0900

    [feat/#112-hashtag-create] 테마파크해시태그 생성 api 구현 - 병합오류수정

--- themepark-service/src/main/java/com/business/themeparkservice/themepark/domain/repository/ThemeparkHashtagRepository.java
+++ themepark-service/src/main/java/com/business/themeparkservice/themepark/domain/repository/ThemeparkHashtagRepository.java
@@ -1,9 +1,4 @@
 package com.business.themeparkservice.themepark.domain.repository;
 
-import com.business.themeparkservice.themepark.domain.entity.ThemeparkHashtagEntity;
-
-import java.util.List;
-
 public interface ThemeparkHashtagRepository {
-    void saveAll(List<ThemeparkHashtagEntity> themeparkHashtagEntityList);
 }

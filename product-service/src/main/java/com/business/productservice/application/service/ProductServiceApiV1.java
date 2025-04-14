892e4b50be9715922fc5d58d44328b8f879d3123
dowon0113 <dowon1235@naver.com>
2025-04-14 16:52:46 +0900

    [feat/#132-product-update] 상품 정보 수정 비즈니스 로직 구현
    - db에서 일치하는 id값 찾은 후 더티체킹으로 정보 업데이트

--- product-service/src/main/java/com/business/productservice/application/service/ProductServiceApiV1.java
+++ product-service/src/main/java/com/business/productservice/application/service/ProductServiceApiV1.java
@@ -1,8 +1,10 @@
 package com.business.productservice.application.service;
 
 import com.business.productservice.application.dto.request.ReqProductPostDTOApiV1;
+import com.business.productservice.application.dto.request.ReqProductPutDTOApiV1;
 import com.business.productservice.application.dto.response.ResProductGetByIdDTOApiV1;
 import com.business.productservice.application.dto.response.ResProductPostDTOApiV1;
+import com.business.productservice.application.dto.response.ResProductPutDTOApiV1;
 import jakarta.validation.Valid;
 
 import java.util.UUID;
@@ -11,4 +13,6 @@ public interface ProductServiceApiV1 {
     ResProductPostDTOApiV1 postBy(@Valid ReqProductPostDTOApiV1 reqDto);
 
     ResProductGetByIdDTOApiV1 getBy(UUID id);
+
+    ResProductPutDTOApiV1 putBy(UUID id, ReqProductPutDTOApiV1 dto);
 }
--- product-service/src/main/java/com/business/productservice/application/service/ProductServiceImplApiV1.java
+++ product-service/src/main/java/com/business/productservice/application/service/ProductServiceImplApiV1.java
@@ -1,8 +1,10 @@
 package com.business.productservice.application.service;
 
 import com.business.productservice.application.dto.request.ReqProductPostDTOApiV1;
+import com.business.productservice.application.dto.request.ReqProductPutDTOApiV1;
 import com.business.productservice.application.dto.response.ResProductGetByIdDTOApiV1;
 import com.business.productservice.application.dto.response.ResProductPostDTOApiV1;
+import com.business.productservice.application.dto.response.ResProductPutDTOApiV1;
 import com.business.productservice.application.exception.ProductExceptionCode;
 import com.business.productservice.domain.product.entity.ProductEntity;
 import com.business.productservice.infrastructure.persistence.product.ProductJpaRepository;
@@ -34,6 +36,25 @@ public class ProductServiceImplApiV1 implements ProductServiceApiV1{
         ProductEntity productEntity = productRepository.findById(id)
                 .orElseThrow(() -> new CustomException(ProductExceptionCode.PRODUCT_NOT_FOUND));
         return ResProductGetByIdDTOApiV1.of(productEntity);
+    }
+
+    @Override
+    public ResProductPutDTOApiV1 putBy(UUID id, ReqProductPutDTOApiV1 dto) {
+
+        ProductEntity productEntity = productRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ProductExceptionCode.PRODUCT_NOT_FOUND));
+
+        productEntity.update(
+                dto.getProduct().getName(),
+                dto.getProduct().getDescription(),
+                dto.getProduct().getProductType(),
+                dto.getProduct().getPrice(),
+                dto.getProduct().getEventStartAt(),
+                dto.getProduct().getEventEndAt(),
+                dto.getProduct().getLimitQuantity(),
+                dto.getProduct().getProductStatus()
+        );
+        return ResProductPutDTOApiV1.of(productEntity);
     }
 
 }

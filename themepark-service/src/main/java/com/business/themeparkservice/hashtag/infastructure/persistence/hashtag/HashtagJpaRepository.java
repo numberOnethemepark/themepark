d75ca22458212c6068be58c5a7be780d03b8c5de
devdaeun <ekdms008@gmail.com>
2025-04-14 10:47:58 +0900

    [feat/#119-hashtag-search] 해시태그 검색 기능 구현 - controller 수정 - service 로직추가 - response dto 수정 - 테스트코드 수정

--- themepark-service/src/main/java/com/business/themeparkservice/hashtag/infastructure/persistence/hashtag/HashtagJpaRepository.java
+++ themepark-service/src/main/java/com/business/themeparkservice/hashtag/infastructure/persistence/hashtag/HashtagJpaRepository.java
@@ -1,12 +1,15 @@
 package com.business.themeparkservice.hashtag.infastructure.persistence.hashtag;
 
-import com.business.themeparkservice.hashtag.domain.repository.HashtagRepository;
 import com.business.themeparkservice.hashtag.domain.entity.HashtagEntity;
+import com.business.themeparkservice.hashtag.domain.repository.HashtagRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface HashtagJpaRepository extends JpaRepository<HashtagEntity, UUID>, HashtagRepository {
+public interface HashtagJpaRepository extends
+        JpaRepository<HashtagEntity, UUID>, HashtagRepository, QuerydslPredicateExecutor<HashtagEntity> {
+
 }

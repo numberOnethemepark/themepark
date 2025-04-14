e051692b3e969920d469743b612508db902e0e23
devdaeun <ekdms008@gmail.com>
2025-04-14 12:13:14 +0900

    [feat/#118-themepark-read] 테마파크 단건조회 기능 구현 - controller 수정 - service 로직추가 - response dto 수정 - 테스트코드 수정

--- themepark-service/src/main/java/com/business/themeparkservice/themepark/application/service/ThemeparkServiceApiV1.java
+++ themepark-service/src/main/java/com/business/themeparkservice/themepark/application/service/ThemeparkServiceApiV1.java
@@ -1,10 +1,15 @@
 package com.business.themeparkservice.themepark.application.service;
 
 import com.business.themeparkservice.themepark.application.dto.request.ReqThemeparkPostDTOApiV1;
+import com.business.themeparkservice.themepark.application.dto.response.ResThemeparkGetByIdDTOApiV1;
 import com.business.themeparkservice.themepark.application.dto.response.ResThemeparkPostDTOApiv1;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 public interface ThemeparkServiceApiV1 {
     ResThemeparkPostDTOApiv1 postBy(@Valid ReqThemeparkPostDTOApiV1 reqDto);
+
+    ResThemeparkGetByIdDTOApiV1 getBy(UUID id);
 }

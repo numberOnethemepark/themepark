a2fa0217e6c626f51736bd92d799dc72607a3910
dowon0113 <dowon1235@naver.com>
2025-04-15 13:10:16 +0900

    [feat/#142-product-search] 조건 기반 상품 조회 비즈니스 로직 구현
    - Querydsl Predicate 적용

--- product-service/src/main/java/com/business/productservice/application/service/ProductServiceImplApiV1.java
+++ product-service/src/main/java/com/business/productservice/application/service/ProductServiceImplApiV1.java
@@ -3,6 +3,7 @@ package com.business.productservice.application.service;
 import com.business.productservice.application.dto.request.ReqProductPostDTOApiV1;
 import com.business.productservice.application.dto.request.ReqProductPutDTOApiV1;
 import com.business.productservice.application.dto.response.ResProductGetByIdDTOApiV1;
+import com.business.productservice.application.dto.response.ResProductGetDTOApiV1;
 import com.business.productservice.application.dto.response.ResProductPostDTOApiV1;
 import com.business.productservice.application.dto.response.ResProductPutDTOApiV1;
 import com.business.productservice.application.exception.ProductExceptionCode;
@@ -11,8 +12,11 @@ import com.business.productservice.domain.product.entity.StockEntity;
 import com.business.productservice.infrastructure.persistence.product.ProductJpaRepository;
 import com.business.productservice.infrastructure.persistence.product.StockJpaRepository;
 import com.github.themepark.common.application.exception.CustomException;
+import com.querydsl.core.types.Predicate;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -39,6 +43,13 @@ public class ProductServiceImplApiV1 implements ProductServiceApiV1{
         ProductEntity productEntity = productRepository.findById(id)
                 .orElseThrow(() -> new CustomException(ProductExceptionCode.PRODUCT_NOT_FOUND));
         return ResProductGetByIdDTOApiV1.of(productEntity);
+    }
+
+    @Override
+    public ResProductGetDTOApiV1 getBy(Predicate predicate, Pageable pageable){
+
+        Page<ProductEntity> productEntityPage = productRepository.findAll(predicate, pageable);
+        return ResProductGetDTOApiV1.of(productEntityPage);
     }
 
     @Override

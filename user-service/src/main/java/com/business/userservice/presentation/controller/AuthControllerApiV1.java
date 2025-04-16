23144e86bba35bac4078e58c1cc59eb55ec23301
문고은 <>
2025-04-16 22:43:12 +0900

    [feat #174-auth-jwt] 게스트 로그인 관련 파일 삭제

--- user-service/src/main/java/com/business/userservice/presentation/controller/AuthControllerApiV1.java
+++ user-service/src/main/java/com/business/userservice/presentation/controller/AuthControllerApiV1.java
@@ -1,8 +1,6 @@
 package com.business.userservice.presentation.controller;
 
-import com.business.userservice.application.dto.request.ReqAuthPostGuestLoginDTOApiV1;
 import com.business.userservice.application.dto.request.ReqAuthPostJoinDTOApiV1;
-import com.business.userservice.application.dto.response.ResAuthPostGuestLoginDTOApiV1;
 import com.business.userservice.application.dto.response.ResAuthPostJoinDTOApiV1;
 import com.business.userservice.application.service.AuthServiceApiV1;
 import com.github.themepark.common.application.dto.ResDTO;
@@ -10,9 +8,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController

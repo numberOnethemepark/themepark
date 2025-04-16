2f31bb1e2a16bae1709c30b080bcda1be865ca5f
문고은 <>
2025-04-16 16:30:38 +0900

    [refactor #171-user-exception] Auth 및 User 예외 관련 Enum 생성

--- user-service/src/main/java/com/business/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/business/userservice/UserServiceApplication.java
@@ -3,7 +3,7 @@ package com.business.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.business.userservice","com.github.themepark.common"})
 public class UserServiceApplication {
 
     public static void main(String[] args) {

7fcc49dc79674ebac1d97860e04a73612b0d4d9e
문고은 <>
2025-04-17 21:41:59 +0900

    [feat #199-user] Manager 생성 서비스 및 컨트롤러 구현

--- user-service/src/main/java/com/business/userservice/application/service/AuthServiceApiV1.java
+++ user-service/src/main/java/com/business/userservice/application/service/AuthServiceApiV1.java
@@ -1,10 +1,14 @@
 package com.business.userservice.application.service;
 
 import com.business.userservice.application.dto.request.ReqAuthPostJoinDTOApiV1;
+import com.business.userservice.application.dto.request.ReqAuthPostManagerJoinDTOApiV1;
 import com.business.userservice.application.dto.response.ResAuthPostJoinDTOApiV1;
+import com.business.userservice.application.dto.response.ResAuthPostManagerJoinDTOApiV1;
 
 public interface AuthServiceApiV1 {
     ResAuthPostJoinDTOApiV1 joinBy(ReqAuthPostJoinDTOApiV1 dto);
 
     String refreshToken(String accessToken, String refreshToken);
+
+    ResAuthPostManagerJoinDTOApiV1 managerJoinBy(ReqAuthPostManagerJoinDTOApiV1 dto);
 }

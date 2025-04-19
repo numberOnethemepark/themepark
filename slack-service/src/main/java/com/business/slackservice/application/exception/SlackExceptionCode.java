17871f8e6c13e70372e6d7421abf88267dd0ef77
문고은 <>
2025-04-19 20:52:43 +0900

    [feat #218-slack-event-type] 슬랙 이벤트 유형 관련 예외 코드 생성

---
+++ slack-service/src/main/java/com/business/slackservice/application/exception/SlackExceptionCode.java
@@ -0,0 +1,25 @@
+package com.business.slackservice.application.exception;
+
+import com.github.themepark.common.application.exception.ExceptionCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Slack Event Type - S1XX
+ * Slack Template - S2XX
+ * Slack - S3XX
+ */
+
+@Getter
+@RequiredArgsConstructor
+public enum SlackExceptionCode implements ExceptionCode {
+    SLACK_EVENT_TYPE_NOT_FOUND("S101", "슬랙 이벤트 타입을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    SLACK_EVENT_TYPE_NAME_DUPLICATED("S102", "이미 존재하는 슬랙 이벤트 타입 이름입니다.", HttpStatus.BAD_REQUEST),
+
+    ;
+
+    private final String code;
+    private final String message;
+    private final HttpStatus status;
+}

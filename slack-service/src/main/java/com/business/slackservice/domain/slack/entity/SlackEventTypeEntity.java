daf895bf5c8f524a03a2e12c10d5061f20ec5a2b
문고은 <>
2025-04-19 20:53:40 +0900

    [feat #218-slack-event-type] SlackEventTypeEntity 수정

--- slack-service/src/main/java/com/business/slackservice/domain/slack/entity/SlackEventTypeEntity.java
+++ slack-service/src/main/java/com/business/slackservice/domain/slack/entity/SlackEventTypeEntity.java
@@ -1,5 +1,6 @@
 package com.business.slackservice.domain.slack.entity;
 
+import com.github.themepark.common.domain.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,17 +9,40 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "p_slack_event_types")
 @Entity
-public class SlackEventTypeEntity {
+public class SlackEventTypeEntity extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "slack_event_type_id")
     private UUID id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
+
+    @Builder
+    private SlackEventTypeEntity(
+        String name
+    ) {
+        this.name = name;
+    }
+
+    public static SlackEventTypeEntity create(
+        String name
+    ) {
+        return SlackEventTypeEntity.builder()
+            .name(name)
+            .build();
+    }
+
+    public void update(String name) {
+        this.name = name;
+    }
 }

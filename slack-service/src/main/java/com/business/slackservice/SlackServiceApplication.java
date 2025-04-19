4be6f4fa1e84fd16b7c0399fb54c58ca16d098e2
문고은 <>
2025-04-19 20:56:46 +0900

    [feat #218-slack-event-type] scanBasePackages 추가

--- slack-service/src/main/java/com/business/slackservice/SlackServiceApplication.java
+++ slack-service/src/main/java/com/business/slackservice/SlackServiceApplication.java
@@ -3,7 +3,7 @@ package com.business.slackservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.business.slackservice","com.github.themepark.common"})
 public class SlackServiceApplication {
 
     public static void main(String[] args) {

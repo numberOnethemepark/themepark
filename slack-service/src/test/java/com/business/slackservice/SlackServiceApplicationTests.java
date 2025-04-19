3ab340c28b6fc98f154ac73e910e6c9145ae8e94
문고은 <>
2025-04-19 16:44:31 +0900

    [feat #216-slack] slack 서비스 세팅

---
+++ slack-service/src/test/java/com/business/slackservice/SlackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.slackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

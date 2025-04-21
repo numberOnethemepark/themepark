95d96bcd656f9e8ecb402bc0e7ca07ef891f5bdc
문고은 <>
2025-04-21 16:27:54 +0900

    [test #240-slack] Slack CRD 테스트 코드 작성 및 컨트롤러 파일명 변경

--- slack-service/src/main/java/com/business/slackservice/presentation/controller/SlackControllerApiV1.java
+++ slack-service/src/main/java/com/business/slackservice/presentation/controller/SlackControllerApiV1.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/slacks")
-public class SlackControllerV1 {
+public class SlackControllerApiV1 {
 
     private final SlackServiceApiV1 slackServiceApiV1;
 

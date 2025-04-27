8b4af19f8f1f41606ebe23e60e08189b5091ea7c
devdaeun <ekdms008@gmail.com>
2025-04-28 02:24:17 +0900

    [refactor/#264-waiting-kafka] 대기열 호출 kafka 설정 - dto 수정 - kafka config 포트번호 수정

--- themepark-service/src/main/java/com/business/themeparkservice/waiting/config/KafkaConfig.java
+++ themepark-service/src/main/java/com/business/themeparkservice/waiting/config/KafkaConfig.java
@@ -17,7 +17,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:8088");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 //        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);

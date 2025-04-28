831a8635e302769d07884b0724ea3394cdc3435a
문고은 <>
2025-04-28 22:28:22 +0900

    [fix/#299-user] 블랙리스트 조회 메서드 반환 타입 수정

--- user-service/src/main/java/com/business/userservice/domain/user/repository/BlacklistRepository.java
+++ user-service/src/main/java/com/business/userservice/domain/user/repository/BlacklistRepository.java
@@ -1,9 +1,10 @@
 package com.business.userservice.domain.user.repository;
 
+import java.sql.Timestamp;
 import java.util.Optional;
 
 public interface BlacklistRepository {
     void save(String userId);
-    Optional<String> findByUserId(String userId);
+    Optional<Timestamp> findByUserId(String userId);
     void delete(String userId);
 }

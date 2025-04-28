3bbf1f2acde723b1df098827a0275ea75709832a
devdaeun <ekdms008@gmail.com>
2025-04-28 14:14:13 +0900

    [rename/#284-waiting] 대기열 버전관리 - feignclient 연결진행 v1 분리

--- themepark-service/src/main/java/com/business/themeparkservice/waiting/infastructure/kafka/service/SlackKafkaServiceApiV1.java
+++ themepark-service/src/main/java/com/business/themeparkservice/waiting/infastructure/kafka/service/SlackKafkaServiceApiV1.java
@@ -1,4 +1,4 @@
-package com.business.themeparkservice.waiting.infastructure.feign.service;
+package com.business.themeparkservice.waiting.infastructure.kafka.service;
 
 import com.business.themeparkservice.themepark.application.exception.ThemeparkExceptionCode;
 import com.business.themeparkservice.themepark.domain.entity.ThemeparkEntity;
@@ -6,7 +6,7 @@ import com.business.themeparkservice.themepark.infastructure.persistence.themepa
 import com.business.themeparkservice.waiting.application.exception.WaitingExceptionCode;
 import com.business.themeparkservice.waiting.domain.entity.WaitingEntity;
 import com.business.themeparkservice.waiting.domain.vo.WaitingStatus;
-import com.business.themeparkservice.waiting.infastructure.feign.dto.request.ReqSlackPostDTOApiV1;
+import com.business.themeparkservice.waiting.infastructure.kafka.dto.request.ReqSlackPostDTOApiV1;
 import com.business.themeparkservice.waiting.infastructure.persistence.waiting.WaitingJpaRepository;
 import com.github.themepark.common.application.exception.CustomException;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +18,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class SlackKafkaService {
+public class SlackKafkaServiceApiV1 {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
     private final WaitingJpaRepository waitingRepository;
